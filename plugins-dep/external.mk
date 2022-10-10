38c04867c422743afa57b59b2413e735fd92d9b4
Giedrius <giedrius@blokas.io>
2022-10-10 09:52:57 +0300

    Use buildroot-2021.11.3, the last one to support python2 - some plugins must be updated to use python3 instead to be compatible with newer BR versions

--- plugins-dep/external.mk
+++ plugins-dep/external.mk
@@ -1,13 +1,10 @@
 MPB_PACKAGES := $(wildcard $(BR2_EXTERNAL_MOD_PLUGIN_BUILDER_PATH)/package/*/*.mk)
 MPB_PACKAGES += $(wildcard $(BR2_EXTERNAL_MOD_PLUGIN_BUILDER_PATH)/../plugins/package/*/*.mk)
 
-# 2022.05.2 buildroot contains some packages that are defined here, causing conflicts - prefer using BR2 provided packages.
-ifeq ($(shell expr "$(BR2_VERSION)" \>= "2022.02.5"),1)
+# 2021.11.3 buildroot contains some packages that are defined here, causing conflicts - prefer using BR2 provided packages.
+ifeq ($(shell expr "$(BR2_VERSION)" \>= "2021.11.3"),1)
 
-MPB_LOCAL_PACKAGES_TO_IGNORE := fluidsynth lilv lv2 python-pillow serd sord sratom
-
-# These depend on python2, ignore them.
-MPB_LOCAL_PACKAGES_TO_IGNORE += lvtk lvtk-1
+MPB_LOCAL_PACKAGES_TO_IGNORE := fluidsynth python-pillow # These are available in 2022.02.5: lilv lv2 serd sord sratom
 
 define remove-dep
 	MPB_PACKAGES := $(filter-out $(BR2_EXTERNAL_MOD_PLUGIN_BUILDER_PATH)/package/$1/$1.mk, $(MPB_PACKAGES))
